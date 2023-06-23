aef193d44d749e09e4ecf99326c761a99ae35d38
Charlie <charlie.duffett@icloud.com>
2023-06-23 14:57:46 +0100

    setup base gamemode and player controller. then made it so the maze cube now rotates on mouse movement

---
+++ Source/BallInHoopGame/MazeGamemode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BallInHoopGameGameModeBase.h"
+#include "MazeGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLINHOOPGAME_API AMazeGamemode : public ABallInHoopGameGameModeBase
+{
+	GENERATED_BODY()
+	
+};
